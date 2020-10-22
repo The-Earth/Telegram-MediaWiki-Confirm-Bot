7bc6da1d5bc1c39be1eb45910a44f88fafe3ff23
The-Earth <tigerzeng97@gmail.com>
2020-10-22 23:01:17 +0800

    [Add](main) Add newly confirmed logging

--- main.py
+++ main.py
@@ -190,6 +190,7 @@ def confirm_button(query: catbot.CallbackQuery):
 
     try:
         if entry.confirmed:
+            log(config['messages']['confirm_log'].format(tg_id=entry.telegram_id, wp_id=entry.wikimedia_username))
             bot.send_message(query.msg.chat.id, text=config['messages']['confirm_complete'])
             if entry.restricted_until <= time.time() + 30:
                 bot.lift_restrictions(config['group'], query.from_.id)
